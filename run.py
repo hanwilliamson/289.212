c449b4556eaef189a67ca0e01287a1aacc26ccbe
Han <hannah225.w@gmail.com>
2020-08-04 11:44:41 +1200

    class work

--- run.py
+++ run.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
 app = Flask(__name__)
-
-burgers = [
-    ['Classic Burger', '$4.99'],
-    ['Cheese Burger', '$5.99'],
-    ['Chicken Burger', '$5.99'],
-    ['Double Burger', '$6.99']
-]
-
-drinks = [
-    ['Cola', '$0.99'],
-    ['Ginger Ale', '$0.99'],
-    ['Beer', '$1.99'],
-    ['Coffee', '$1.99']
-]
-
-sides = [
-    ['Fries', '$1.99'],
-    ['Onion Rings' '$1.99'],
-    ['Mushrooms' '$1.99'],
-    ['Salad' '$1.99']
-]
+MENUDB = 'menu.db'
 
 @app.route('/')
 def index():
+    burgers = []
+    db = sqlite3.connect(MENUDB)
+    print(db)
+    cur = db.execute('SELECT burger,price FROM burgers')
+    for row in cur:
+        burgers.append(list(row))
+
+    drinks = []
+    db = sqlite3.connect(MENUDB)
+    print(db)
+    cur = db.execute('SELECT drink,price FROM drinks')
+    for row in cur:
+        drinks.append(list(row))
+
+    sides = []
+    db = sqlite3.connect(MENUDB)
+    print(db)
+    cur = db.execute('SELECT side,price FROM sides')
+    for row in cur:
+        sides.append(list(row))
+
+
+    db.close()
+
     return render_template('index.html',
                             disclaimer='may contain traces of nuts',
                             burgers=burgers,
